f9acf773fe3800ee6ffa640b7e90473491e4384a
fernaldya <fwirjowerdojo@gmail.com>
2026-05-17 15:46:56 +0700

    Paging issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ const queryClient = new QueryClient()
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route
